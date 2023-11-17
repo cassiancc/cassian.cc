06e7e0f99b54bb1ec086a402929fd577d5b4d9ee
CG <congodsted@protonmail.com>
2023-11-16 23:24:13 -0500

    Homepage carousels update
    IA and IoT projects have been moved to carousels.
    
    A new drop shadow effect is present on the project cards.
    
    README added.
    
    I've wrote up additional context for all the projects. I'm looking to rework the read more on the cards, but that'll be later.
    
    Next update tackles whatever this did to the mobile view.

---
+++ README.md
@@ -0,0 +1,7 @@
+# congodsted.com
+congodsted.com acts as a personal portfolio page.
+
+# Libraries
+GlideJS by Jędrzej Chałubek, licensed under the terms of the MIT License: https://glidejs.com
+
+Font Awesome 4, used under the SIL OFL 1.1 License https://fontawesome.com/v4/license
---
+++ js/index.js
@@ -0,0 +1,84 @@
+let height = 591;
+let peekVal = document.querySelector(".item").clientWidth
+
+function openCollapsible(button) {
+    height = 591
+    button.classList.toggle("active");
+    let readmore = button.nextElementSibling;
+    let content = readmore.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+        readmore.innerHTML = `<br>read more...`
+    } else {
+        readmore.innerHTML = "<br>read less..."
+        content.style.display = "block";
+        document.querySelectorAll(".item").forEach(function(element) {
+            if (element.clientHeight > height ) {
+                height = element.clientHeight
+            }
+    })
+    }
+    
+    document.querySelectorAll(".item").forEach(function(element) {
+        element.style.minHeight = `${height}px`
+        
+    })
+    
+};
+
+            
+var iaGlide = new Glide('#ia-glide', {
+    autoplay: 4000,
+    type: "carousel",
+    animationDuration: 2000,
+    animationTimingFunc: 'linear',
+    peek: {
+    before: peekVal,
+    after: peekVal
+}})
+var iotGlide = new Glide('#iot-glide', {
+    autoplay: 4000,
+    type: "carousel",
+    animationDuration: 2000,
+    animationTimingFunc: 'linear',
+    peek: {
+    before: peekVal,
+    after: peekVal
+}})
+peek()
+
+function peek () {
+    peekVal = document.querySelector(".carousel").clientWidth
+    let carousel = document.querySelector(".carousel").clientWidth
+    console.log("CAROUSEL", carousel)
+    if (carousel > 600) {
+        peekVal = peekVal / 5
+        console.log("small")
+        
+
+    }
+    else {
+        peekVal = Math.sqrt(peekVal)
+        peekVal * 6
+
+    }
+    iotGlide.update({
+        peek: {
+        before: peekVal,
+        after: peekVal
+        }
+    })
+    iaGlide.update({
+        peek: {
+        before: peekVal,
+        after: peekVal
+        }
+    })
+    return peekVal
+}
+
+
+iaGlide.mount()
+iotGlide.mount()
+peek()
+window.addEventListener("resize", peek);
